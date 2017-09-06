9e5ec47436afbadb42f12d02ed1c49bfda3edecc
LD <981824007@qq.com>
2017-09-06 20:41:18 +0800

    first commit game_project

---
+++ GitTest/Assets/Scripts/Camer_Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camer_Move : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed = 10;
+    private float move_z;
+    private float move_x;
+    private Transform m_transform;
+	void Awake () {
+        m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move_z = Input.GetAxis("Horizontal")*speed;
+        move_x = Input.GetAxis("Vertical")*speed;
+        m_transform.Translate(new Vector3(move_z,0,move_x)*Time.deltaTime);
+	}
+}
